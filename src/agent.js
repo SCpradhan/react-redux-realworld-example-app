18909f73af4d1483b83e7e8282ba1ab563dee8b9
joyrahaASC <joy.raha@ascendion.com>
2026-07-02 04:14:12 -0500

    Auto-generated RCA fix by Deep Level Agent

--- src/agent.js
+++ src/agent.js
@@ -65,7 +65,13 @@ const Articles = {
   update: article =>
     requests.put(`/articles/${article.slug}`, { article: omitSlug(article) }),
   create: article =>
-    requests.post('/articles', { article })
+    requests.post('/articles', { article }),
+  saveDraft: article =>
+    requests.post('/articles', { article: Object.assign({}, article, { published: false }) }),
+  updateDraft: article =>
+    requests.put(`/articles/${article.slug}`, { article: Object.assign({}, omitSlug(article), { published: false }) }),
+  publishDraft: article =>
+    requests.put(`/articles/${article.slug}`, { article: Object.assign({}, omitSlug(article), { published: true }) })
 };
 
 const Comments = {
