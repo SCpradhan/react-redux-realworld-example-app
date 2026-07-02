a659b1bfd0f43e77b8d16c97b88350a49f22d69b
joyrahaASC <joy.raha@ascendion.com>
2026-07-02 13:29:20 -0500

    Auto-generated RCA fix by Deep Level Agent

--- src/reducers/editor.js
+++ src/reducers/editor.js
@@ -17,6 +17,7 @@ export default (state = {}, action) => {
         title: action.payload ? action.payload.article.title : '',
         description: action.payload ? action.payload.article.description : '',
         body: action.payload ? action.payload.article.body : '',
+        isDraft: action.payload ? action.payload.article.isDraft : false,
         tagInput: '',
         tagList: action.payload ? action.payload.article.tagList : []
       };
